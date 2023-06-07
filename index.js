b7007574b5b0628bb5df7b56a0307debc0853645
Ceclea <cecleavictor@gmail.com>
2023-06-07 09:12:42 +0300

    am adaugat functionalul unei baze de date MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb+srv://cecleavictor:T1fEwF0cPXJBw3dI@cluster0.yjovoqs.mongodb.net/?retryWrites=true&w=majority')
+    .then(()=>console.log("DB OK"))
+    .catch(()=> console.log("DB ERROR"))
 
 const app = express()
 
@@ -9,9 +15,20 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/auth/login',(req, res)=>{
+
     console.log(req.body)
+
+    const token = jwt.sign({
+        email: req.body.email,
+            fullName: "Ceclea Victor"
+    },
+        'secret123'
+    )
+
+
     res.json({
-        succes:true
+        succes:true,
+        token
     })
 })
 
