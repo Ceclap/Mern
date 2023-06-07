cdda15a24521e5910b5dd67e5650636652249bd8
Ceclea <cecleavictor@gmail.com>
2023-06-07 20:13:36 +0300

    am realizat inregistarea cu salvarea datelor in BD

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
+
 import {validationResult} from 'express-validator'
 import {registerValidator} from "./validations/auth.js"
 
-mongoose.connect('mongodb+srv://cecleavictor:T1fEwF0cPXJBw3dI@cluster0.yjovoqs.mongodb.net/?retryWrites=true&w=majority')
+import UserModel from "./models/users.js"
+
+mongoose.connect('mongodb+srv://cecleavictor:T1fEwF0cPXJBw3dI@cluster0.yjovoqs.mongodb.net/body?retryWrites=true&w=majority')
     .then(()=>console.log("DB OK"))
     .catch(()=> console.log("DB ERROR"))
 
@@ -16,15 +20,36 @@ app.get('/',(req,res)=>{
     res.send('HelloWorld')
 })
 
-app.post('/auth/register',registerValidator,(req, res)=>{
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors.array())
-    }
+app.post('/auth/register',registerValidator,async (req, res) => {
+   try{
+       const errors = validationResult(req)
+       if (!errors.isEmpty()) {
+           return res.status(400).json(errors.array())
+       }
 
-    res.json({
-        succes:true,
-    })
+       const password = req.body.password
+       const salt = await bcrypt.genSalt(10);
+       const passwordHash = await bcrypt.hash(password, salt)
+
+       const doc = new UserModel({
+           email: req.body.email,
+           fullName: req.body.fullName,
+           passwordHash,
+           avatarUrl: req.body.avatarUrl,
+
+       })
+
+       const user = await doc.save()
+
+
+       res.json(user)
+   }catch (err){
+       console.log(err)
+       res.status(500).json({
+           message:"Nu sa primit sa va conectati"
+       })
+   }
+
 })
 
 
