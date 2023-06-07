be8dbd8346f820282969be5bbb956393373291fc
Victor Ceclea <126653016+Ceclap@users.noreply.github.com>
2023-06-07 22:19:27 +0300

    Revert "Am finisat logarea si registrarea"

--- index.js
+++ index.js
@@ -29,33 +29,20 @@ app.post('/auth/register',registerValidator,async (req, res) => {
 
        const password = req.body.password
        const salt = await bcrypt.genSalt(10);
-       const hash = await bcrypt.hash(password, salt)
+       const passwordHash = await bcrypt.hash(password, salt)
 
        const doc = new UserModel({
            email: req.body.email,
            fullName: req.body.fullName,
-           hash,
+           passwordHash,
            avatarUrl: req.body.avatarUrl,
 
        })
 
        const user = await doc.save()
 
-       const token = jwt.sign({
-           id: user._id
-            },
-           "secret123",
-           {
-               expiresIn: '30d'
-           }
-           )
 
-       const {passwordHash, ...userData} = user._doc
-
-       res.json({
-           ...user._doc,
-           token
-       })
+       res.json(user)
    }catch (err){
        console.log(err)
        res.status(500).json({
@@ -65,41 +52,12 @@ app.post('/auth/register',registerValidator,async (req, res) => {
 
 })
 
-app.post('/auth/login',async (req, res)=>{
-    try{
-        const user = await UserModel.findOne({email:req.body.email})
-        if(!user){
-            return res.status(404).json({
-                message: "User nu a fost gasit"
-            })
-        }
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-        if(!isValidPass){
-            return res.status(400).json({
-                message: "Parola sau Login gresit"
-            })
-        }
-        const token = jwt.sign({
-                id: user._id
-            },
-            "secret123",
-            {
-                expiresIn: '30d'
-            })
 
-        const {passwordHash, ...userData} = user._doc
-
-        res.json({
-            ...user._doc,
-            token
-        })
-
-    }catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message:"Nu sa primit sa va autendificati"
-        })
-    }
+app.post('/auth/login',(req, res)=>{
+    console.log(req.body)
+    res.json({
+        succes:true
+    })
 })
 
 app.listen(3000, (err)=>{
